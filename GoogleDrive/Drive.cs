42392587e6d4c3c2567df4c5dbdfbdb101d957df
Eddy Fishman <eddy.fishman@gmail.com>
2018-07-28 18:32:40 +0300

    Normal slide links

--- GoogleDrive/Drive.cs
+++ GoogleDrive/Drive.cs
@@ -15,6 +15,7 @@ using System.Threading;
 
 namespace GoogleDrive
 {
+    #region Class Drive
     public class Drive
     {
         #region Class Members
@@ -23,6 +24,7 @@ namespace GoogleDrive
         SlidesService slidesService;
         static string[] Scopes = { DriveService.Scope.DriveReadonly, SlidesService.Scope.Presentations };
         static string ApplicationName = "Google Drive";
+        JsonSerializer jsonSerializer;
 
         public List<string> Presentations { get; }
 
@@ -46,7 +48,7 @@ namespace GoogleDrive
                     new FileDataStore(credPath, true)).Result;
             }
 
-            // Create Drive API service.
+            // Create Drive, Slides API services.
             driveService = new DriveService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
@@ -62,13 +64,15 @@ namespace GoogleDrive
             #endregion
 
             #region Try load presentations list from local cache
+
+            jsonSerializer = new JsonSerializer();
+
             // deserialize JSON directly from a file
             if (File.Exists(ConfigurationManager.AppSettings["PresentationsListCache"]))
             {
                 using (StreamReader file = File.OpenText(ConfigurationManager.AppSettings["PresentationsListCache"]))
                 {
-                    JsonSerializer serializer = new JsonSerializer();
-                    Presentations = (List<string>)serializer.Deserialize(file, typeof(List<string>));
+                    Presentations = (List<string>)jsonSerializer.Deserialize(file, typeof(List<string>));
                 }
             }
             else
@@ -76,6 +80,7 @@ namespace GoogleDrive
                 Presentations = new List<string>();
             }
             #endregion
+
         }
         #endregion
 
@@ -118,14 +123,13 @@ namespace GoogleDrive
         /// </summary>
         public void SavePresentationsList()
         {
-            JsonSerializer serializer = new JsonSerializer();
-            serializer.Converters.Add(new JavaScriptDateTimeConverter());
-            serializer.NullValueHandling = NullValueHandling.Ignore;
+            jsonSerializer.Converters.Add(new JavaScriptDateTimeConverter());
+            jsonSerializer.NullValueHandling = NullValueHandling.Ignore;
 
             using (StreamWriter sw = new StreamWriter(ConfigurationManager.AppSettings["PresentationsListCache"]))
             using (JsonWriter writer = new JsonTextWriter(sw))
             {
-                serializer.Serialize(writer, Presentations);
+                jsonSerializer.Serialize(writer, Presentations);
             }
 
         }
@@ -135,49 +139,83 @@ namespace GoogleDrive
         /// 1) Adds an empty slide in the end, if it does not exist ("Empty board")
         /// 2) For each slide (except the last one "empty board"):
         ///     a) Delete existing speaker notes
-        ///     b) Add Links to: "Next Slide" (to skip animated hints/solutions, "Last Slide" (empty board)
+        ///     b) Add Links to: "Prev Slide", "Next Slide" (to skip animated hints/solutions, "Last Slide" (empty board)
         ///     c) Adjust slide number text box
         /// 3) For the last slide: add "TOC": a link to each slide (except this last slide)
         /// </summary>
         /// <param name="presentationId"></param>
         public void ProcessPresentation(string presentationId)
         {
+            var lastSlidelink = new Link() { RelativeLink = "LAST_SLIDE" };
+            var nextSlidelink = new Link() { RelativeLink = "NEXT_SLIDE" };
+            var prevSlidelink = new Link() { RelativeLink = "PREV_SLIDE" };
+            var firstSlidelink = new Link() { RelativeLink = "FIRST_SLIDE" };
+
             var presentationRequest = slidesService.Presentations.Get(presentationId);
             var presentation = presentationRequest.Execute();
 
-            var myBatchRequest = new MyBatchRequest(slidesService, presentationId);
-            if (presentation.Slides[0].SlideProperties.NotesPage.PageElements[1].Shape.Text != null)
-            {
-                myBatchRequest.AddDeleteTextRequest(presentation.Slides[0].SlideProperties.NotesPage.PageElements[1].ObjectId);
-                var a = myBatchRequest.Execute();
-            }
+            var myBatchRequest = new MyBatchRequest(slidesService, presentationId, presentation.Slides[0]);
+            myBatchRequest.AddDeleteTextRequest();
+
+            var currentStartIndex = 0;
+            var lastSlideText = ConfigurationManager.AppSettings["LastSlideText"] + "\t";
+            myBatchRequest.AddInsertTextRequest(lastSlideText, currentStartIndex);
+            myBatchRequest.AddUpdateTextStyleRequest(currentStartIndex, currentStartIndex + lastSlideText.Length, lastSlidelink);
+            currentStartIndex += lastSlideText.Length;
+
+            var nextSlideText = ConfigurationManager.AppSettings["NextSlideText"] + "\t";
+            myBatchRequest.AddInsertTextRequest(nextSlideText, currentStartIndex);
+            myBatchRequest.AddUpdateTextStyleRequest(currentStartIndex, currentStartIndex + nextSlideText.Length, nextSlidelink);
+            currentStartIndex += nextSlideText.Length;
+
+            var a = myBatchRequest.Execute();
         }
 
         #endregion
 
     }
+    #endregion
 
+    #region Class MyBatchRequest
     public class MyBatchRequest
     {
+        #region Class Members
+
         SlidesService slidesService;
         BatchUpdatePresentationRequest batchUpdatePresentationRequest;
+        Page slide;
         string presentationId;
 
-        public MyBatchRequest(SlidesService slidesService, string presentationId)
+        #endregion
+
+        #region C'Tor/D'Tor
+        public MyBatchRequest(SlidesService slidesService, string presentationId, Page slide)
         {
             this.slidesService = slidesService;
             batchUpdatePresentationRequest = new BatchUpdatePresentationRequest();
             batchUpdatePresentationRequest.Requests = new List<Request>();
             this.presentationId = presentationId;
+            this.slide = slide;
         }
+        #endregion
 
-        public void AddDeleteTextRequest(string objectId)
+        #region Methods
+
+        /// <summary>
+        /// Adds a DeleteText request to an object - to delete its entire text
+        /// </summary>
+        public void AddDeleteTextRequest()
         {
+            if (slide.SlideProperties.NotesPage.PageElements[1].Shape.Text == null)
+            {
+                return;
+            }
+
             batchUpdatePresentationRequest.Requests.Add(new Request()
             {
                 DeleteText = new DeleteTextRequest()
                 {
-                    ObjectId = objectId,
+                    ObjectId = slide.SlideProperties.NotesPage.PageElements[1].ObjectId,
                     TextRange = new Range
                     {
                         Type = "ALL"
@@ -186,12 +224,71 @@ namespace GoogleDrive
             });
         }
 
+        /// <summary>
+        /// Adds an InsertText request to an object
+        /// </summary>
+        /// <param name="objectId"></param>
+        public void AddInsertTextRequest(string text, int insertionIndex)
+        {
+            batchUpdatePresentationRequest.Requests.Add(new Request()
+            {
+                InsertText = new InsertTextRequest()
+                {
+                    ObjectId = slide.SlideProperties.NotesPage.PageElements[1].ObjectId,
+                    Text = text,
+                    InsertionIndex = insertionIndex
+                }
+            });
+        }
+
+        /// <summary>
+        /// Adds an update text style request to a specific text with optional linking
+        /// </summary>
+        /// <param name="startIndex"></param>
+        /// <param name="endIndex"></param>
+        /// <param name="link"></param>
+        public void AddUpdateTextStyleRequest(int startIndex, int endIndex, Link link)
+        {
+            var textStyle = JsonConvert.DeserializeObject<TextStyle>(ConfigurationManager.AppSettings["TextStyle"]);
+            var fields = ConfigurationManager.AppSettings["TextStyleFields"];
+
+            textStyle.Link = link;
+            fields += ",link";
+
+            batchUpdatePresentationRequest.Requests.Add(new Request()
+            {
+                UpdateTextStyle = new UpdateTextStyleRequest()
+                {
+                    ObjectId = slide.SlideProperties.NotesPage.PageElements[1].ObjectId,
+                    Style = textStyle,
+                    TextRange = new Range()
+                    {
+                        Type = "FIXED_RANGE",
+                        StartIndex = startIndex,
+                        EndIndex = endIndex
+                    },
+                    Fields = fields
+                }
+            });
+        }
+
+        /// <summary>
+        /// Executes the requests added to the list
+        /// </summary>
+        /// <returns></returns>
         public BatchUpdatePresentationResponse Execute()
         {
-            var batchUpdateRequest = slidesService.Presentations.BatchUpdate(batchUpdatePresentationRequest, presentationId);
-            return batchUpdateRequest.Execute();
+            if (batchUpdatePresentationRequest.Requests != null && batchUpdatePresentationRequest.Requests.Count > 0)
+            {
+                var batchUpdateRequest = slidesService.Presentations.BatchUpdate(batchUpdatePresentationRequest, presentationId);
+                return batchUpdateRequest.Execute();
+            }
+            return null;
         }
+        
+        #endregion
     }
+    #endregion
 }
 
 
